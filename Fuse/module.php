bbf81664c14902e4abe30d4f698563256cd4620b
Andreas Seemueller <andreas.seemueller@gmail.com>
2023-08-03 22:08:57 +0200

    sdf

--- Fuse/module.php
+++ Fuse/module.php
@@ -94,9 +94,12 @@ class Fuse extends IPSModule {
 
 		// Clean old message registration
 		$messagesList = $this->GetMessageList();
-		foreach ($messagesList as $currentMessage) {
+		foreach ($messagesList as $currentMessageVarId => $currentMessageIDs) {
 
-			$this->UnregisterMessage($currentMessage, VM_UPDATE);
+			foreach ($currentMessageIDs as $currentMessageID) {
+
+				$this->UnregisterMessage($currentMessageVarId, $currentMessageID);
+			}
 		}
 		
 		$this->RegisterMessage($this->ReadPropertyInteger("SourceVariable"), VM_UPDATE);
